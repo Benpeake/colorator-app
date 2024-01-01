7c40d6c285448994a6333672066769f4eb38d17f
Benpeake <dearbenp@gmail.com>
2024-01-01 21:06:19 +0000

    mobile vh test

--- src/components/color-gen-interface/index.js
+++ src/components/color-gen-interface/index.js
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Controller from "./color-gen-controller";
 import Panel from "./color-gen-panel";
 import "./interface.css";
-
 
 function Interface({
   colors,
@@ -23,40 +22,60 @@ function Interface({
   copySuccess,
   setCopySuccess
 }) {
+  const [vh, setVh] = useState(window.innerHeight * 0.01);
+
+  useEffect(() => {
+    const updateVh = () => {
+      setVh(window.innerHeight * 0.01);
+    };
+
+    window.addEventListener("resize", updateVh);
+    updateVh();
+
+    return () => {
+      window.removeEventListener("resize", updateVh);
+    };
+  }, []);
 
   return (
     <>
-    <Controller
-    generateRandomColor={generateRandomColor}
-    undo={undo}
-    redo={redo}
-    addColorPanel={addColorPanel}
-    colors={colors}
-    history={history}
-    historyIndex={historyIndex}
-    setDisplayAddPalette={setDisplayAddPalette}
-    setDisplaySignUp={setDisplaySignUp}
-    token={token}
-  />
-    <section className="interface-container">
-      {colors.map((color, index) => (
-        <Panel
-          colors={colors}
-          key={index}
-          color={color.color}
-          name={color.name}
-          fontColor={color.fontColor}
-          index={index}
-          handleMoveColor={handleMoveColor}
-          removeColorPanel={removeColorPanel}
-          handleLockStatus={handleLockStatus}
-          updateColor={updateColor}
-          tempUpdateColor={tempUpdateColor}
-          copySuccess={copySuccess}
-          setCopySuccess={setCopySuccess}
-        />
-      ))}
-    </section>
+      <Controller
+        generateRandomColor={generateRandomColor}
+        undo={undo}
+        redo={redo}
+        addColorPanel={addColorPanel}
+        colors={colors}
+        history={history}
+        historyIndex={historyIndex}
+        setDisplayAddPalette={setDisplayAddPalette}
+        setDisplaySignUp={setDisplaySignUp}
+        token={token}
+      />
+      <section
+        className="interface-container"
+        style={{
+          minHeight: `calc(${vh * 100} - 11.1vw)`,
+          height: `calc(${vh * 100} - 11.1vw)`,
+        }}
+      >
+        {colors.map((color, index) => (
+          <Panel
+            colors={colors}
+            key={index}
+            color={color.color}
+            name={color.name}
+            fontColor={color.fontColor}
+            index={index}
+            handleMoveColor={handleMoveColor}
+            removeColorPanel={removeColorPanel}
+            handleLockStatus={handleLockStatus}
+            updateColor={updateColor}
+            tempUpdateColor={tempUpdateColor}
+            copySuccess={copySuccess}
+            setCopySuccess={setCopySuccess}
+          />
+        ))}
+      </section>
     </>
   );
 }
